3beb98eee5e1433ce5fb5c7ee88fb6770360abab
Mikey Thien <mikey.thien@Administrative-2.local>
2018-12-07 15:56:57 -0500

    Improved GUI

--- GUIbowelmovement.py
+++ GUIbowelmovement.py
@@ -4,7 +4,9 @@ def submit():
 	print("Submit Pressed")
 	list.append(slide1.get())
 	list.append(slide2.get())
-	list.append(slide3.get())
+	list.append(ent1.get())
+	list.append(ent2.get())
+	
 	print(list)
 
 def change(*args):
@@ -13,70 +15,75 @@ list = []
 
 root = tk.Tk()
 root.title("Bowel Movement Tracker")
+root.configure(background="mediumslateblue")
 
 #------------------- LABELS -------------------
+label00 = tk.Label(root, text = "                     ", background = "darkslateblue")
+label00.grid(row = 0, column = 0, sticky = "NESW")
 
-label = tk.Label(root, text = "Bowel Movements Per Week")
-label.grid(row = 0, column = 0, columnspan = 2)
+label0 = tk.Label(root, text = "Bowel Movement Tracker", background = "darkslateblue", foreground = "white", height = 2)
+label0.grid(row = 0, column = 1, sticky = "NESW")
 
-label = tk.Label(root, text = "Water Drank Per Week (mL)")
-label.grid(row = 3, column = 0, columnspan = 2)
+label1 = tk.Label(root, text = "Bowel Movements Per Week", background = "darkslateblue", foreground = "white", height = 2)
+label1.grid(row = 1, column = 0, sticky = "NESW")
 
-label = tk.Label(root, text = "Food Eaten")
-label.grid(row = 6, column = 0, columnspan = 1)
+label2 = tk.Label(root, text = "Water Drank Per Week (L)", background = "darkslateblue", foreground = "white", height = 2)
+label2.grid(row = 3, column = 0, sticky = "NESW")
+
+label3 = tk.Label(root, text = "Weight (pounds)", background = "darkslateblue", foreground = "white", height = 2)
+label3.grid(row = 5, column = 0, sticky = "NESW")
+
+label4 = tk.Label(root, text = "Type of Food Eaten", background = "darkslateblue", foreground = "white", height = 2)
+label4.grid(row = 7, column = 0, sticky = "NESW")
+
+label5 = tk.Label(root, text = "Amount of Fibers in food (grams)", background = "darkslateblue", foreground = "white", height = 2)
+label5.grid (row = 9, column = 0, sticky = "NESW")
+
+label6 = tk.Label(root, text = "", background = "darkslateblue")
+label6.grid(row = 10, column = 0, sticky = "NESW")
 
 #-------------------- ENT --------------------
 
-slide1 = tk.Scale(root, from_=0, to=30, resolution= 1, orient=tk.HORIZONTAL)
-slide1.grid(row = 1, column = 0, columnspan = 1)
+slide1 = tk.Scale(root, from_=0, to=50, resolution= 1, orient=tk.HORIZONTAL)
+slide1.grid(row = 1, column = 1)
+slide1.configure(background="mediumslateblue", foreground = "white")
 
 slide2 = tk.Scale(root, from_=0, to=30, resolution= 1, orient=tk.HORIZONTAL)
-slide2.grid(row = 4, column = 0, columnspan = 1)
+slide2.grid(row = 3, column = 1)
+slide2.configure(background="mediumslateblue", foreground = "white")
 
-#slide3 = tk.Scale(root, from_=0, to=30, resolution= 1, orient=tk.HORIZONTAL)
-#slide3.grid(row = 7, column = 0, columnspan = 1)
+slide3 = tk.Scale(root, from_=0, to=300, resolution= 1, orient=tk.HORIZONTAL)
+slide3.grid(row = 5, column = 1)
+slide3.configure(background="mediumslateblue", foreground = "white")
 
 #-------------------- Buttons ------------------
 
-btn = tk.Button(root, text = "Submit", command = submit)
-btn.grid (row = 1, column = 2)
-
-#-------------------- Drop Down List ------------------
-#Step 1: Create a list of everythign that is in the drop 
-DROPDOWNLIST = ["Option 1","Option 2","Option 3"]
-#Step 2: Create a variable to trace/track the current option selelcted
-ddvar = tk.StringVar(root)
-#Step 3: Set the inital value of ddvar (what is your drop down initalized to)
-ddvar.set(DROPDOWNLIST[0])
-#Step 3A: Attach a function that runs when I change ddvar
-ddvar.trace("w",change)
-#Step 4: Create the drow down menu
-omenu = tk.OptionMenu(root, ddvar, DROPDOWNLIST[0], DROPDOWNLIST[1],DROPDOWNLIST[2])
-#Step 5: Pack it in the window
-omenu.grid(row = 7, column = 0)
+btn1 = tk.Button(root, text = "Submit", command = submit, height = 2, width = 10)
+btn1.grid (row = 10, column = 1)
+btn1.configure(background="mediumslateblue")
 
 
+#-------------------- Food Eaten ---------------
 
+ent1 = tk.Entry(root)
+ent1.grid(row = 7, column = 1)
+ent1.configure(background="lightgray")
 
+ent2 = tk.Entry(root)
+ent2.grid(row = 9, column = 1, columnspan = 1)
+ent2.configure(background="lightgray")
 
+#-------------------- Text Box -----------------
 
+textboxRESULT = tk.Text(root, height = 10, width = 50, borderwidth = 3, relief = tk.GROOVE)
+textboxRESULT.insert(tk.INSERT,"The Results Will Be Shown Here " + " \n")
+textboxRESULT.config(state = "disabled")
+textboxRESULT.grid(row = 11, column = 1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+textbox0 = tk.Text(root, height = 10, width = 40, borderwidth = 3, relief = tk.GROOVE)
+textbox0.config(state = "disabled")
+textbox0.grid(row = 11, column = 0)
+#-------------------Program--------------------
 
 
 
